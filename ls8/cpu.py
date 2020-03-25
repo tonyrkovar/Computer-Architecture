9b8db9e0e191718cea4608a5a0cc69e80e632829
Tony <tonyrkovar@gmail.com>
2020-03-25 14:08:47 -0500

    feat: day 1 and 2 mvp

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,41 +2,56 @@
 
 import sys
 
+HLT = 0b00000001
+PRN = 0b01000111
+LDI = 0b10000010
+MUL = 0b10100010
+PSH = 0b01000101
+POP = 0b01000110
+
+
 class CPU:
     """Main CPU class."""
 
     def __init__(self):
-        """Construct a new CPU."""
-        pass
+        self.ram = [0] * 256
+        self.pc = 0
+        self.reg = [0] * 8
+        self.branchtable = {HLT: self.op_hlt,
+                            PRN: self.op_prn,
+                            LDI: self.op_ldi,
+                            MUL: self.op_mul,
+                            PSH: self.op_push,
+                            POP: self.op_pop}
+        self.running = True
 
-    def load(self):
+    def load(self, file):
         """Load a program into memory."""
-
         address = 0
+        with open(file) as f:
+            for instruction in f:
+                li = instruction.strip().split('#')
+                line = li[0].strip()
+                if line == '':
+                    continue
+                else:
+                    print(line)
+                    instruction = int(line, 2)
+                    # print(instruction, "Without leading 0s")
+                    # print(f'{instruction:08d}, with leading 0s')
+                    self.ram[address] = instruction
+                    address += 1
+
+        program = []
 
         # For now, we've just hardcoded a program:
-
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
-
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
-
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
+        # elif op == "SUB": etc
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -48,8 +63,8 @@ class CPU:
 
         print(f"TRACE: %02X | %02X %02X %02X |" % (
             self.pc,
-            #self.fl,
-            #self.ie,
+            # self.fl,
+            # self.ie,
             self.ram_read(self.pc),
             self.ram_read(self.pc + 1),
             self.ram_read(self.pc + 2)
@@ -58,8 +73,46 @@ class CPU:
         for i in range(8):
             print(" %02X" % self.reg[i], end='')
 
-        print()
+    def ram_read(self, address):
+        value = self.ram[address]
+        return value
+
+    def ram_write(self, address, value):
+        self.ram[address] = value
+
+    def op_prn(self, operand_a, operand_b):
+        value = self.ram_read(self.pc + 1)
+        print(value)
+        self.pc += 2
+        print("End print")
+        return value
+
+    def op_mul(self, operand_a, operand_b):
+        value = operand_a * operand_b
+        self.pc += 2
+        print("End mul", value)
+        return value
+
+    def op_ldi(self, operand_a, operand_b):
+        print('op_ldi')
+        self.ram_write(operand_a, operand_b)
+        self.pc += 3
+        print("End LDI")
+
+    def op_hlt(self, operand_a, operand_b):
+        print('Stopping')
+        self.running = False
 
     def run(self):
         """Run the CPU."""
-        pass
+
+        while self.running:
+            IR = self.ram_read(self.pc)
+            operand_a = self.ram_read(self.pc + 1)
+            operand_b = self.ram_read(self.pc + 2)
+            if int(bin(IR), 2) in self.branchtable:
+                print(IR)
+                self.branchtable[IR](operand_a, operand_b)
+            else:
+                print("Not valid")
+                self.running = False
